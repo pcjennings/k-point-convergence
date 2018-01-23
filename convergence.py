2961054bdb0353845a82800a949713cafd38fec2
Paul C. Jennings <jennings.p.c@gmail.com>
2018-01-23 10:51:05 -0800

    add optimization loop

--- convergence.py
+++ convergence.py
@@ -1,10 +1,31 @@
 """Main function for convergence tracker."""
+from subprocess import call
 
 
 class ConvergenceTrackerQE(object):
     def __init__(self, input_file, ecutwfc):
         self.input_file = input_file
         self.ecutwfc = ecutwfc
+
+    def optimize(self, cutoff=0.01):
+        """Function to optimize kpoint parameter."""
+        # Commit initial input.
+        call('cp pw-scf_0.out pw-scf.out', shell=True)
+        energy = self._scrape_output('pw-scf.out')
+
+        converged = False
+        index = 1  # just for test purposes.
+        while not converged:
+            kpt = self._edit_input()
+            call('cp pw-scf_{}.out pw-scf.out'.format(index), shell=True)
+            new_energy = self._scrape_output('pw-scf.out')
+            if abs(energy - new_energy) < cutoff:
+                converged = True
+            else:
+                energy = new_energy
+                index += 1
+
+        return kpt
 
     def _edit_input(self):
         """Function to edit qe input script."""
@@ -21,11 +42,13 @@ class ConvergenceTrackerQE(object):
                 k_points_edit = index + 1
         # Edit desired on values.
         self._set_ecutwfc(ecutwfc_edit)
-        self._iterate_kpoints(k_points_edit)
+        new_kpt = self._iterate_kpoints(k_points_edit)
 
         # Write out the new file.
-        with open('pw_scf_tmp.in', 'w') as f:
+        with open('pw_scf.in', 'w') as f:
             f.write(''.join(self.input_data))
+
+        return new_kpt
 
     def _set_ecutwfc(self, edit):
         """Function to edit ecutwfc."""
@@ -39,6 +62,8 @@ class ConvergenceTrackerQE(object):
         for k in range(3):
             s[k] = str(int(s[k]) + 1)
         self.input_data[edit] = ' '.join(s)
+
+        return ' '.join(s[:3])
 
     def _scrape_output(self, output_file):
         """Function to get total energy from calculation output."""
@@ -60,5 +85,5 @@ class ConvergenceTrackerQE(object):
 
 if __name__ == '__main__':
     ct = ConvergenceTrackerQE('pw_scf.in', 80)
-    ct._edit_input()
-    ct._scrape_output('pw-scf.out')
+    print('current kpoints: {}'.format(ct._edit_input()))
+    print('current energy: {}'.format(ct._scrape_output('pw-scf.out')))
