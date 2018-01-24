c9df0777af92ec5dec219ad277c8774e10045346
Paul C. Jennings <jennings.p.c@gmail.com>
2018-01-23 21:05:08 -0800

    add comments.

--- convergence.py
+++ convergence.py
@@ -3,7 +3,20 @@ from subprocess import call
 
 
 class ConvergenceTracker(object):
-    def __init__(self, input_file, output_file, kinetic_energy):
+    """Parent class for the convergence tracker."""
+
+    def __init__(self, input_file, output_file, kinetic_energy=None):
+        """Initialize class.
+
+        Parameters
+        ----------
+        input_file : str
+            Path to user defined input file.
+        output_file : str
+            Expected name of output file.
+        kinetic_energy : int
+            The kinetic energy cutoff for the wavefunctions.
+        """
         self.input_file = input_file
         self.output_file = output_file
         self.kinetic_energy = kinetic_energy
@@ -13,7 +26,24 @@ class ConvergenceTracker(object):
             self.input_data = [line for line in f if not line.isspace()]
 
     def optimize(self, cutoff, param_list, initialize_kpts=False):
-        """Function to optimize kpoint parameter."""
+        """Function to optimize kpoint parameter.
+
+        Parameters
+        ----------
+        cutoff : float
+            Convergence criteria for change in total energy between sampling.
+        param_list : list
+            A list of parameters to change.
+        initialize_kpts : bool
+            Define whether to use the user defined K-points from the input
+            script. If False, start from [1 1 1].
+
+        Returns
+        -------
+        kpts : str
+           The string representation of the converged k-point dimensions
+           (e.g. 5 5 5).
+        """
         if initialize_kpts:
             startk = self.get_start_params()
         else:
@@ -55,23 +85,58 @@ class ConvergenceTracker(object):
         return kpts
 
     def edit_input(self, param_list, var_list):
-        """Function to edit qe input script."""
+        """Function to edit qe input script.
+
+        Parameters
+        ----------
+        param_list : list
+            A list of parameters to change.
+        var_list : list
+            A list of variables corresponding to the same order as the
+            param_list.
+        """
         self.update_input(param_list, var_list)
         # Write out the new file.
         with open('pw_scf.in', 'w') as f:
             f.write(''.join(self.input_data))
 
     def scrape_output(self, output_file, param='energy'):
-        """Function to get total energy from calculation output."""
+        """Function to get total energy from calculation output.
+
+        Parameters
+        ----------
+        output_file : str
+            Expected output filename.
+        param : str
+            Parameter to return from output. Default is energy.
+
+        Returns
+        -------
+        res : float
+            The result from the output.
+        """
         with open(output_file, 'r') as f:
             output_data = f.readlines()
 
         self.check_calculation_convergence(output_data)
-        res = eval('self.get_{}(output_data)'.format(param))
+        try:
+            res = eval('self.get_{}(output_data)'.format(param))
+        except AttributeError:
+            raise NotImplementedError(
+                'Finding parameter {} not implemented.'.format(param))
 
         return res
 
     def _iterate_kpoints(self, original, step=1):
+        """Function to increase the number of k-points.
+
+        Parameters
+        ----------
+        original : str
+            The original k-point dimensions.
+        step : int
+            The step size to increase k-points.
+        """
         k = original.split(' ')
         for i in range(3):
             k[i] = str(int(k[i]) + step)
@@ -80,15 +145,36 @@ class ConvergenceTracker(object):
 
 
 class ConvergenceTrackerQE(ConvergenceTracker):
+    """QE specific class for the convergence tracker."""
+
     def __init__(self, input_file, output_file, kinetic_energy):
+        """Initialize class.
+
+        Parameters
+        ----------
+        input_file : str
+            Path to user defined input file.
+        output_file : str
+            Expected name of output file.
+        kinetic_energy : int
+            The kinetic energy cutoff for the wavefunctions.
+        """
         ConvergenceTracker.__init__(self, input_file, output_file,
                                     kinetic_energy)
 
     def update_input(self, param_list, var_list):
-        """Function to edit qe input script."""
+        """Function to edit qe input script.
+
+        Parameters
+        ----------
+        param_list : list
+            A list of parameters to change.
+        var_list : list
+            A list of variables corresponding to the same order as the
+            param_list.
+        """
         # Find the correct lines to edit values.
         for index, line in enumerate(self.input_data):
-            # NOTE could be outside the loop for kpoints
             if 'ecutwfc' in line:
                 kinetic_energy_edit = index
             elif 'K_POINTS' in line:
@@ -98,7 +184,13 @@ class ConvergenceTrackerQE(ConvergenceTracker):
             eval('self._set_{0}({0}_edit, "{1}")'.format(p, v))
 
     def check_calculation_convergence(self, output_data):
-        """Function to check that the calculation has converged."""
+        """Function to check that the calculation has converged.
+
+        Parameters
+        ----------
+        output_data : list
+            Data read in from the output file.
+        """
         converged = False
         rev = output_data[::-1]
         for i in rev:
@@ -110,7 +202,13 @@ class ConvergenceTrackerQE(ConvergenceTracker):
                     raise AssertionError('Calculation did not converge.')
 
     def get_energy(self, output_data):
-        """Function to get total energy from calculation output."""
+        """Function to get total energy from calculation output.
+
+        Parameters
+        ----------
+        output_data : list
+            Data read in from the output file.
+        """
         rev = output_data[::-1]
         for i in rev:
             if 'total energy              =' in i:
@@ -124,13 +222,29 @@ class ConvergenceTrackerQE(ConvergenceTracker):
                 return ' '.join(kpoints[:3])
 
     def _set_kinetic_energy(self, edit, energy):
-        """Function to edit ecutwfc."""
+        """Function to edit ecutwfc.
+
+        Parameters
+        ----------
+        edit : int
+            Line index to edit.
+        energy : int
+            Kinetic energy cutoff for the wavefunctions.
+        """
         s = self.input_data[edit].split(' ')
         s[-1] = '{}\n'.format(energy)
         self.input_data[edit] = ' '.join(s)
 
     def _set_kpoints(self, edit, kpts):
-        """Function to iterate over k-points."""
+        """Function to iterate over k-points.
+
+        Parameters
+        ----------
+        edit : int
+            Line index to edit.
+        kpts : str
+            The k-point dimensions e.g. 1 1 1.
+        """
         s1 = self.input_data[edit].split(' ')
         s2 = kpts.split(' ')
         s = s2[:3] + s1[3:]
